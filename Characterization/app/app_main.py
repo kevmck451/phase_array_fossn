15dc8fcc5bcbae16b2bc4e707c8e5af40237eed9
KevMcK <kmcrunk@gmail.com>
2024-05-24 16:53:50 -0500

    characterization experiment initial commit

--- Characterization/app/app_main.py
+++
@@ -1,9 +0,0 @@
-from app.View.Main_Window import Main_Window
-from app.Controller.controller import Controller
-
-if __name__ == "__main__":
-    controller = Controller()
-    gui = Main_Window(controller.handle_event)
-    controller.set_gui(gui)
-    gui.mainloop()
-
