7d58c0054646be79bd0b6afe60e2e266be32f968
KevMcK <kmcrunk@gmail.com>
2024-08-10 00:52:29 -0500

    config updates

--- Network/nixos/configuration.nix
+++ Network/nixos/configuration.nix
@@ -21,6 +21,13 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  environment.systemPackages = with pkgs; [
+    pkgs.nano
+    pkgs.git
+    pkgs.tmux
+    pkgs.htop
+  ];
+
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
@@ -28,39 +35,36 @@
   i18n.defaultLocale = "enCA.UTF-8";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-   users.users.admin = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
+  users.users.admin = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
 
-     ];
-   };
+    ];
+  };
 
-   users.users.kevmck = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
+  security.sudo.extraRules= [
+    { users = [ "admin" ];
+      commands = [
+        { command = "ALL";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 
-     ];
-   };
+  nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-   environment.systemPackages = with pkgs; [
-     pkgs.nano
-     pkgs.git
-     pkgs.tmux
-     pkgs.htop
-     ];
+  # Configure Keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
 
   # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
-
-   # Configure Keymap in X11
-   services.xserver = {
-     layout = "us";
-     xkbVariant = "";
-   };
+  services.openssh.enable = true;
+  services.qemuGuest.enable = true;
+  programs.nix-ld.enable = true;
 
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.state>
   system.stateVersion = "23.11"; # Did you read the comment?
