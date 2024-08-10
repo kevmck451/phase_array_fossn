1da6989da6c0da05549454cc1672341f2b50faf2
KevMcK <kmcrunk@gmail.com>
2024-08-10 02:32:02 -0500

    reorganization and merging

--- Network/nixos/network.nix
+++ Network/nixos/network.nix
@@ -1,4 +1,3 @@
-
 { config, lib, pkgs, ... }:
 
 {
@@ -8,7 +7,7 @@
      hostapd
      dnsmasq
      bridge-utils
-     ];
+  ];
 
   # add wireless service
   hardware.enableRedistributableFirmware = true;
@@ -25,13 +24,30 @@
     wpaPassphrase = "123456";
   };
 
+  # Consolidated network interfaces configuration
+  networking.interfaces = {
+    # Wireless interface setup with static IP
+    wlp1s0u1u4.ipv4.addresses = lib.optionals config.services.hostapd.enable [{
+      address = "10.0.0.1";
+      prefixLength = 24;
+    }];
+    # Ethernet interface static IP
+    end0.ipv4.addresses = [{
+      address = "192.168.0.143";
+      prefixLength = 24;
+    }];
+    # VLANs IP configuration
+    vlan2.ipv4.addresses = [{
+      address = "192.168.2.1";
+      prefixLength = 24;
+    }];
+    vlan3.ipv4.addresses = [{
+      address = "192.168.3.1";
+      prefixLength = 24;
+    }];
+  };
 
-  # set up wireless static IP address
-  networking.interfaces.wlp1s0u1u4.ip4 = lib.mkOverride 0 [ ];
-  networking.interfaces.wlp1s0u1u4.ipv4.addresses = lib.optionals config.services.hostapd.enable [{ address = "10.0.0.1"; prefixLength = 24; }];
-
-
-  # set up wireless DNS
+  # DNS configuration
   services.dnsmasq = lib.optionalAttrs config.services.hostapd.enable {
     enable = true;
     extraConfig = ''
@@ -41,67 +57,32 @@
     '';
   };
 
+  # Firewall configuration
   networking.firewall.allowedUDPPorts = lib.optionals config.services.hostapd.enable [53 67];
   services.haveged.enable = config.services.hostapd.enable;
 
-  # Finally, bridge ethernet and wifi
+  # Bridge configuration
   networking.bridges.br0.interfaces = [ "end0" "wlp1s0u1u4" ];
 
-  # Set Static IP
-  networking = {
-    defaultGateway = {
-        address = "192.168.0.1";
-        interface = "end0";
-    };
-    nameservers = [ "192.168.0.1" ];
-    domain = "pi-nix";
-    search = [ "pi-nix" ];
-    interfaces = {
-        end0.ipv4.addresses = [{
-            address = "192.168.0.143";
-            prefixLength = 24;
-        }];
-        vlan2.ipv4.addresses = [{
-            address = "192.168.2.1";
-            prefixLength = 24;
-        }];
-        vlan3.ipv4.addresses = [{
-            address = "192.168.3.1";
-            prefixLength = 24;
-        }];
-
-
-
-    };
-
-    # Virtual LANS
-    vlans = {
-        vlan2 = {
-            id = 2;
-            interface = "end0";
-        };
-
-        vlan3 = {
-            id = 3;
-            interface = "end0";
-        };
-
-    };
-
+  # Default gateway and nameserver configuration
+  networking.defaultGateway = {
+    address = "192.168.0.1";
+    interface = "end0";
   };
+  networking.nameservers = [ "192.168.0.1" ];
+  networking.domain = "pi-nix";
+  networking.search = [ "pi-nix" ];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  # Virtual LANs configuration
+  networking.vlans = {
+    vlan2 = {
+      id = 2;
+      interface = "end0";
+    };
+    vlan3 = {
+      id = 3;
+      interface = "end0";
+    };
+  };
 
 }
