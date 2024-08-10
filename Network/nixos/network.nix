bcae46c048378ac44aa419c2224379172af3c65c
KevMcK <kmcrunk@gmail.com>
2024-08-10 02:33:46 -0500

    reorganization and merging

--- Network/nixos/network.nix
+++ Network/nixos/network.nix
@@ -14,40 +14,26 @@
   networking.wireless.enable = true;
 
   # set up wireless access point
-  networking.networkmanager.unmanaged = [ "Phased_Array:wlp1s0u1u4" ]
-    ++ lib.optional config.services.hostapd.enable "Phased_Array:${config.services.hostapd.interface}";
+  networking.networkmanager.unmanaged = [ "Phased_Array:wlp1s0u1u4" ];
+
   services.hostapd = {
     enable = true;
-    interface = "wlp1s0u1u4";
-    hwMode = "g";
-    ssid = "Phased_Array";
-    wpaPassphrase = "123456";
+    radios = {
+      wlp1s0u1u4 = {
+        band = "g"; # Equivalent to hwMode = "g"
+        networks = {
+          network1 = {
+            ssid = "Phased_Array";
+            authentication = {
+              wpaPassword = "123456";
+            };
+          };
+        };
+      };
+    };
   };
 
-  # Consolidated network interfaces configuration
-  networking.interfaces = {
-    # Wireless interface setup with static IP
-    wlp1s0u1u4.ipv4.addresses = lib.optionals config.services.hostapd.enable [{
-      address = "10.0.0.1";
-      prefixLength = 24;
-    }];
-    # Ethernet interface static IP
-    end0.ipv4.addresses = [{
-      address = "192.168.0.143";
-      prefixLength = 24;
-    }];
-    # VLANs IP configuration
-    vlan2.ipv4.addresses = [{
-      address = "192.168.2.1";
-      prefixLength = 24;
-    }];
-    vlan3.ipv4.addresses = [{
-      address = "192.168.3.1";
-      prefixLength = 24;
-    }];
-  };
-
-  # DNS configuration
+  # set up wireless DNS
   services.dnsmasq = lib.optionalAttrs config.services.hostapd.enable {
     enable = true;
     extraConfig = ''
@@ -57,12 +43,23 @@
     '';
   };
 
-  # Firewall configuration
   networking.firewall.allowedUDPPorts = lib.optionals config.services.hostapd.enable [53 67];
   services.haveged.enable = config.services.hostapd.enable;
 
   # Bridge configuration
   networking.bridges.br0.interfaces = [ "end0" "wlp1s0u1u4" ];
+
+  # Set Static IP for VLANs (not for bridged interfaces)
+  networking.interfaces = {
+    vlan2.ipv4.addresses = [{
+      address = "192.168.2.1";
+      prefixLength = 24;
+    }];
+    vlan3.ipv4.addresses = [{
+      address = "192.168.3.1";
+      prefixLength = 24;
+    }];
+  };
 
   # Default gateway and nameserver configuration
   networking.defaultGateway = {
