ae4819931c2d31b0dfa7791ce50cf2da3140bdcd
KevMcK <kmcrunk@gmail.com>
2024-08-16 15:36:30 -0500

    auto run

--- Network/nixos/temp_sensor.nix
+++ Network/nixos/temp_sensor.nix
@@ -9,4 +9,18 @@
     ];
 
     hardware.i2c.enable = true;
+
+
+
+  systemd.services.temp-sensor = {
+      description = "Custom NAT setup for wlp1s0u1u4";
+      after = [ "network.target" ];
+      serviceConfig.ExecStart = let
+        coolBmeEnabledPython3 = (pkgs.python3.withPackages (p: [
+            (p.callPackage ./bme280 {})
+        ]));
+      in "${coolBmeEnabledPython3}/bin/python3 ${./../../Temp_Sensor/temp_sensor_server.py}";
+      serviceConfig.Environment = "PYTHONUNBUFFERED=1";
+      wantedBy = [ "multi-user.target" ];
+  };
 }
